8e0e9152cfa0585688308a468246c810aa68f49e
kumararpit135 <kumararpit994>
2024-10-18 09:43:51 +0530

    getElement by ID

---
+++ get Element by Id/index.js
@@ -0,0 +1,15 @@
+// Write your code below:
+const main=document.getElementById("main-heading")
+main.textContent='Fruit World';
+main.style.color='orange';
+const header=document.getElementById("header");
+
+
+header.style.backgroundColor="green"
+header.style.border='3px solid orange';
+
+const basket=document.getElementById("basket-heading");
+basket.style.color='green';
+
+const thank=document.getElementById("thanks");
+thank.innerHTML='<p>Please visit us again</p>';
