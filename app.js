3cb8ae7107c13b796e7ecaee5e4db7b4f6c0d8d0
kumararpit135 <kumararpit994>
2024-11-14 12:32:14 +0530

    dfsa

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-fasfds
+const http = require('http')
+const express=require('express')
+
+const app=express()
+app.use((req,res,next)=>{
+    console.log("i am in muddfbasdhf");
+    next()
+    
+})
+app.use((req,res,next)=>{
+    console.log("fkjsgfjkgfgs")
+    res.send('<h1>fudhfhsdfohsodghosdhgofhsdoghfoghoshgohfodh</h1>')
+})
+app.listen(3000)
