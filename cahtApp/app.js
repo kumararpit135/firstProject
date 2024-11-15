0380506098c6482d73d3fea839004320a42905a6
kumararpit135 <kumararpit994>
2024-11-15 22:47:38 +0530

    change

--- cahtApp/app.js
+++ cahtApp/app.js
@@ -22,6 +22,7 @@ app.get('/login',(req,res,next)=>{
     
 })
 app.post('/chat',(req,res,next)=>{
+    
     console.log('jiii')
     res.redirect('/')
 })
@@ -38,8 +39,21 @@ app.get('/',(req,res,next)=>{
         res.write('<body>');
         res.write('<h1>Welcome to the Chat Room</h1>');
        
-        res.write(`<body>${data}</body>`); // Display the messages stored in the file
-        res.write(`<form action="/message" method="POST"><input type="text" id="message" name="message"><button type="submit">Send</button></form>`);
+        res.write(`<body> ${data}<h4>=><h4></body>`); 
+        res.write(`
+            <form action="/message" method="POST">
+            <input type="text" id="message" name="message">
+            <input type="hidden" id="username" name="username">
+            <button type="submit">Send</button></form>
+            <script>
+                
+                document.querySelector('form').onsubmit = function(event){
+                    const username = localStorage.getItem('username');
+                    document.getElementById('username').value = username;
+                }
+            </script>
+
+        `);
         res.write('</body>');
         res.write('</html>');
         
@@ -47,10 +61,13 @@ app.get('/',(req,res,next)=>{
 })
 app.post('/message',(req,res,next)=>{
     const mes=req.body.message
-    fs.appendFile('input.txt',mes +'\n',(err)=>{
+    const name=req.body.username
+    console.log(name,'hiiiiiiiii')
+    fs.appendFile('input.txt',`${name}:${mes}`,(err)=>{
         if (err){
             res.send(" you caonnot write in the file therei is a problem ")
         }
+        console.log(name,mes)
         res.redirect('/')
 
     })
